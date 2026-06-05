f1d9352632792a0bce648c0e72bffe3984254213
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:33 -0500

    xml commenting

--- src/Soenneker.SimpleIcons.Runners.Enums.Icons/Constants.cs
+++ src/Soenneker.SimpleIcons.Runners.Enums.Icons/Constants.cs
@@ -1,10 +1,28 @@
 namespace Soenneker.SimpleIcons.Runners.Enums.Icons;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The target repository.
+    /// </summary>
     public const string TargetRepository = "soenneker.simpleicons.enums.icons";
+    /// <summary>
+    /// The upstream repository url.
+    /// </summary>
     public const string UpstreamRepositoryUrl = "https://github.com/simple-icons/simple-icons.git";
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.SimpleIcons.Enums.Icons";
+    /// <summary>
+    /// The enum file name.
+    /// </summary>
     public const string EnumFileName = "SimpleIcon.cs";
+    /// <summary>
+    /// The enum type name.
+    /// </summary>
     public const string EnumTypeName = "SimpleIconEnum";
 }
